f509e913cc0855dbd3d435b5cf13c2cf0a6d5e1a
arappold <a_rappold@gmx.at>
2014-07-10 06:34:25 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'encprim',
+    version = '1.0',
+    author = 'Andreas Rappold',
+    author_email = 'a_rappold@gmx.at',
+    description = 'encprim is a serializer for primitive python objects written in pure python',
+    url = 'https://github.com/notEvil/encprim',
+    packages = ['encprim'],
+)
+
